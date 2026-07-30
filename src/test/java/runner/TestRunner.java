47cbafdf5ccd056e71524bb9f71c86a628fc86a6
maruthi <maruthikunuku718@gmail.com>
2026-07-31 04:43:20 +0530

    Initial commit

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,22 @@
+package runner;
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+
+
+@CucumberOptions(features = {"src/test/java/features"} , plugin = {"json:target/cucumber.json", "pretty"},
+        glue = "steps")
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+
+    @Override
+    @DataProvider
+    //@DataProvider (parallel = true) -- For parallel execution support (which is not going to work for our code)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+
+}
